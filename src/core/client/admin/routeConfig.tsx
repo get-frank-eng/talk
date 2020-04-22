e928b58895d7170c184ce658709441df5f95bac7
tessalt <tessathornton@gmail.com>
2020-04-22 09:55:00 -0400

    allow site selection in dashboard

--- src/core/client/admin/routeConfig.tsx
+++ src/core/client/admin/routeConfig.tsx
@@ -26,6 +26,7 @@ import AddSiteRoute from "./routes/Configure/sections/Sites/AddSiteRoute";
 import SiteRoute from "./routes/Configure/sections/Sites/SiteRoute";
 import WebhookEndpointsLayout from "./routes/Configure/sections/WebhookEndpoints/WebhookEndpointsLayout";
 import DashboardRoute from "./routes/Dashboard";
+import SiteDashboardRoute from "./routes/Dashboard/SiteDashboardRoute";
 import ForgotPasswordRoute from "./routes/ForgotPassword";
 import InviteRoute from "./routes/Invite";
 import LoginRoute from "./routes/Login";
@@ -109,6 +110,7 @@ export default makeRouteConfig(
         </Route>
         <Route path="stories" {...StoriesRoute.routeConfig} />
         <Route path="dashboard" {...DashboardRoute.routeConfig} />
+        <Route path="dashboard/:siteID" {...SiteDashboardRoute.routeConfig} />
         <Route path="community" {...CommunityRoute.routeConfig} />
         <Route
           {...createAuthCheckRoute({
